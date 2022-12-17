b79457a816dcd36b00b50560fa17d46b63c8868b
EliasJPeters <eligreenmen@gmail.com>
2022-12-17 00:17:51 -0500

    Added the basics of email functionality to notify users of open classes

---
+++ emailContent.py
@@ -0,0 +1,28 @@
+import smtplib
+from email.message import EmailMessage
+from email.headerregistry import Address
+from email.mime.text import MIMEText
+import ssl
+
+email_sender = "usfclassbot@gmail.com"
+email_password = "tgtnxufmyurgpxxa"
+email_receiver = "eligreenmen@gmail.com"
+
+subject = "Test message subject"
+body = """
+Test message body
+with multiple lines
+to test the newline
+"""
+
+msg = EmailMessage()
+msg["From"] = email_sender
+msg["To"] = email_receiver
+msg["Subject"] = subject
+msg.set_content(body)
+
+context = ssl.create_default_context()
+
+with smtplib.SMTP_SSL("smtp.gmail.com", 465, context = context) as smtp:
+    smtp.login(email_sender, email_password)
+    smtp.sendmail(email_sender, email_receiver, msg.as_string())
