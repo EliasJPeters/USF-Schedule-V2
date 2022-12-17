22ba5da8e53c1503f8630ecfefd0c3bb2ba1422d
EliasJPeters <eligreenmen@gmail.com>
2022-12-17 00:18:48 -0500

    All of UI is working with validation

--- seleniumContents.py
+++ seleniumContents.py
@@ -10,7 +10,7 @@ import time
 
 
 fireFoxOptions = webdriver.FirefoxOptions()
-fireFoxOptions.headless = True
+fireFoxOptions.headless = False
 
 website = "https://usfweb.usf.edu/DSS/StaffScheduleSearch"
 
@@ -21,7 +21,7 @@ def openSite():
     driver.get(website)
     return driver
 
-def inputInformation(driver, term, partTerm, campus, college, department, status, status2, level, crn):
+def inputInformation(driver, term, partTerm, campus, college, department, status, status2, level, crn, subject, number):
     if term != "null":
         select = Select(driver.find_element_by_name("P_SEMESTER")) 
         select.select_by_value(term)
@@ -53,15 +53,44 @@ def inputInformation(driver, term, partTerm, campus, college, department, status
     if level != "null":
         select = Select(driver.find_element_by_name("P_CRSE_LEVL"))
         select.select_by_value(level)
-        
+
     if crn != "null":
         crnspot = driver.find_element_by_name("P_REF")
         crnspot.send_keys(crn)
+
+    if subject != "null":
+        crnspot = driver.find_element_by_name("P_SUBJ")
+        crnspot.send_keys(subject)
+
+    if number != "null":
+        crnspot = driver.find_element_by_name("P_NUM")
+        crnspot.send_keys(number)
+
+    driver.execute_script("window.scrollTo(0,document.body.scrollHeight)")
+    time.sleep(1)
+    search = driver.find_element_by_css_selector("#InnerWrapper > div.container > form > button:nth-child(15)")
+    search.click()
 
 def crnError():
     msg = QMessageBox()
     msg.setIcon(QMessageBox.Critical)
     msg.setText("CRN Error")
     msg.setInformativeText('Please make sure the CRN is 5 digits and numeric.')
+    msg.setWindowTitle("Error")
+    msg.exec()
+
+def subjectError():
+    msg = QMessageBox()
+    msg.setIcon(QMessageBox.Critical)
+    msg.setText("Subject Error")
+    msg.setInformativeText('Please make sure the Subject is 3 letters.       (Ex: ENG)')
+    msg.setWindowTitle("Error")
+    msg.exec()
+
+def numberError():
+    msg = QMessageBox()
+    msg.setIcon(QMessageBox.Critical)
+    msg.setText("Class number Error")
+    msg.setInformativeText('Please make sure the class number is 4 numbers and 1 letter (if applicable).')
     msg.setWindowTitle("Error")
     msg.exec()
