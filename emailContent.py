3c3e48fd3a6d0a339c1e6f91b59e3d648b8847f5
EliasJPeters <eligreenmen@gmail.com>
2022-12-24 18:50:18 -0500

    Added email when seat is detected. This time I'm NOT leaking my email passwork on GitHub :)

--- emailContent.py
+++ emailContent.py
@@ -5,12 +5,31 @@ from email.mime.text import MIMEText
 import ssl
 
 email_sender = "usfclassbot@gmail.com"
-email_password = "tgtnxufmyurgpxxa"
+email_password = ""
 
 def initialEmail(crn, email, courseNumber):
     subject = ("Hello! You've chosen to be notified of seat openings for the class with the CRN of: " + crn)
     body = ("You will be emailed a notification when an open seat is detected for: " + courseNumber + "\n\nThanks for using this program!\n" + 
     "This project was created by Elias Peters. You can get more information from the github here: https://github.com/EliasJPeters/USF-Schedule-V2")
+    
+    email_receiver = email
+    msg = EmailMessage()
+    msg["From"] = email_sender
+    msg["To"] = email_receiver
+    msg["Subject"] = subject
+    msg.set_content(body)
+
+    context = ssl.create_default_context()
+
+    with smtplib.SMTP_SSL("smtp.gmail.com", 465, context = context) as smtp:
+        smtp.login(email_sender, email_password)
+        smtp.sendmail(email_sender, email_receiver, msg.as_string())
+
+def seatEmail(email, courseNumber,openSeats):
+    subject = ("An open seat has been detected!")
+    body = ("An open seat for course: " + courseNumber + " has been detected!\nThe number of seats available: " + openSeats + 
+    "\nAttempt to register for the course ASAP!\n\n" + 
+    "Would you like to stop receiving emails? Just close the program window on your computer!")
     
     email_receiver = email
     msg = EmailMessage()
